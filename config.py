5a7d21545b6fdab184479b4fb336a404f63d4cb3
terabox25 <164458249+terabox25@users.noreply.github.com>
2025-08-27 07:13:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-ADMIN_IDS = [123456789]   # Replace with your Telegram ID
+BOT_TOKEN = "7712967452:AAH-pB8BxHKk-ywX8kX0Kco3sJzaFrkPiyA"
+ADMIN_IDS = [7154097545]   # Replace with your Telegram ID
 QUIZ_ROOT = "quizzes"
 MAX_MCQS = 20
